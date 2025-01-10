27674d7bd01f6f5456cf392b31bb1c43cd20fea4
bildno <gingu33@naver.com>
2025-01-10 17:25:19 +0900

    Board생성,삭제,수정 구현

--- src/components/ServerList.js
+++ src/components/ServerList.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext.js";
 import axios from "axios";
 import { use } from "react";
+import Swal from "sweetalert2";
+import { useServerList } from "../hooks/useServerList.js";
 
 const ServerList = React.memo(
   ({
@@ -13,142 +15,31 @@ const ServerList = React.memo(
     selectedServer,
     onOpenDM,
     onRefreshServers,
+    userList,
   }) => {
     const navigate = useNavigate();
     const { onLogout } = useContext(AuthContext);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [previewImage, setPreviewImage] = useState(null);
-    const [serverName, setServerName] = useState("");
-    const [serverTag, setServerTag] = useState("");
-    const [serverImage, setServerImage] = useState(null);
-    const [contextMenu, setContextMenu] = useState({
-      visible: false,
-      x: 0,
-      y: 0,
-      serverId: null,
-    });
-    const currentUserId = localStorage.getItem("email");
 
-    const handleLogout = useCallback(async () => {
-      try {
-        if (!onLogout) {
-          console.error("Logout function not available");
-          return;
-        }
-
-        localStorage.removeItem("token");
-        localStorage.removeItem("userName");
-        localStorage.removeItem("userId");
-        localStorage.removeItem("userRole");
-
-        await onLogout();
-        navigate("/");
-      } catch (error) {
-        console.error("Logout failed:", error);
-        // 사용자에게 에러 메시지를 표시할 수 있습니다
-      }
-    }, [onLogout, navigate]);
-
-    const handleCreateServer = () => {
-      setIsModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-      setIsModalOpen(false);
-      setPreviewImage(null);
-      setServerName("");
-      setServerTag("");
-      setServerImage(null);
-    };
-
-    const handleImageChange = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        setServerImage(file);
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setPreviewImage(reader.result);
-        };
-        reader.readAsDataURL(file);
-      }
-    };
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-      try {
-        const formData = new FormData();
-        formData.append("title", serverName);
-        formData.append("tag", serverTag);
-
-        if (serverImage != null) {
-          formData.append("serverImg", serverImage);
-        }
-
-        const response = await axios.post(
-          "http://localhost:8181/server/servers",
-          formData,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        console.log(response);
-
-        if (response.status === 200) {
-          console.log("성공");
-
-          handleCloseModal();
-          if (onRefreshServers) {
-            await onRefreshServers();
-          }
-        }
-      } catch (error) {
-        console.error("서버 생성 실패:", error);
-        alert("서버 생성에 실패했습니다.");
-      }
-    };
-
-    const handleContextMenu = useCallback((e, serverId) => {
-      e.preventDefault();
-      setContextMenu({
-        visible: true,
-        x: e.pageX,
-        y: e.pageY,
-        serverId,
-      });
-    }, []);
-
-    const handleDeleteServer = async (serverId) => {
-      try {
-        const response = await axios.delete(
-          `http://localhost:8181/server/servers?id=${serverId}`,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
-
-        if (response.status === 200) {
-          onSelectServer(null);
-          setContextMenu({ visible: false, x: 0, y: 0, serverId: null });
-
-          if (onRefreshServers) {
-            await onRefreshServers();
-          }
-
-          alert("삭제되었습니다.");
-          return;
-        }
-      } catch (error) {
-        console.error("서버 삭제 실패:", error);
-        alert("서버 삭제에 실패했습니다.");
-      }
-      setContextMenu({ visible: false, x: 0, y: 0, serverId: null });
-    };
+    const {
+      isModalOpen,
+      previewImage,
+      serverName,
+      serverTag,
+      serverImage,
+      contextMenu,
+      handleSubmit,
+      handleImageChange,
+      handleCreateServer,
+      userEmail,
+      handleLogout,
+      setContextMenu,
+      handleLeaveServer,
+      handleDeleteServer,
+      handleContextMenu,
+      handleCloseModal,
+      setServerName,
+      setServerTag,
+    } = useServerList(onLogout, onSelectServer, onRefreshServers);
 
     useEffect(() => {
       const handleClick = () =>
@@ -173,20 +64,18 @@ const ServerList = React.memo(
               selectedServer === server.id ? "selected" : ""
             }`}
             onClick={() =>
-              onSelectServer(server.id, server.title, server.userId)
+              onSelectServer(server.id, server.title, server.role, server.tag)
             }
-            onContextMenu={(e) =>
-              server.userId === currentUserId && handleContextMenu(e, server.id)
-            }
+            onContextMenu={(e) => handleContextMenu(e, server)}
           >
             {server.serverImg ? (
               <img
-                src={`http://localhost:8181${server.serverImg}`}
-                alt={server.title}
+                src={server.serverImg}
+                alt={"업따"}
                 className="server-image"
               />
             ) : (
-              server.title
+              server.serverImg
             )}
           </div>
         ))}
@@ -259,9 +148,15 @@ const ServerList = React.memo(
               left: contextMenu.x,
             }}
           >
-            <button onClick={() => handleDeleteServer(contextMenu.serverId)}>
-              서버 삭제
-            </button>
+            {contextMenu.serverEmail === userEmail ? (
+              <button onClick={() => handleDeleteServer(contextMenu.serverId)}>
+                서버 삭제
+              </button>
+            ) : (
+              <button onClick={() => handleLeaveServer(contextMenu.serverId)}>
+                서버 탈퇴
+              </button>
+            )}
           </div>
         )}
         <div style={{ marginTop: "auto" }}>
