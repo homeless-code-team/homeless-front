299a0a35cadb3837938d81eef5a1624d66650a1b
Chani's <psc0729@naver.com>
2025-02-04 14:30:07 +0900

    임시수정

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
